ae08d4a4257d9114f22ca9e276a331dc6edd8a33
Evgeniy <lifesup@gmail.com>
2020-07-26 11:05:53 +0700

    + two-fer

---
+++ go/two-fer/two_fer.go
@@ -0,0 +1,29 @@
+/*
+`Two-fer` or `2-fer` is short for two for one. One for you and one for me.
+
+Given a name, return a string with the message:
+
+```text
+One for X, one for me.
+```
+
+Where X is the given name.
+
+However, if the name is missing, return the string:
+
+```text
+One for you, one for me.
+```
+ */
+
+package twofer
+
+import "fmt"
+
+// Build string with specified name
+func ShareWith(name string) string {
+	if name == "" {
+		name = "you"
+	}
+	return fmt.Sprintf("One for %s, one for me.", name)
+}
