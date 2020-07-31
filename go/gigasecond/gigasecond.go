bd8ca8656d058f15d12edc3efa22d80e89811531
Evgeniy <lifesup@gmail.com>
2020-07-31 21:56:05 +0700

    * side ex: gigasecond, performance improvements 35ns => 6ns

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,13 +1,15 @@
-// Package gigasecond contains method for work with gigasecond duration.
+// Package gigasecond contains method and constant for work with gigasecond duration.
 // A gigasecond is 10^9 (1,000,000,000) seconds.
 package gigasecond
 
 import (
-	"math"
 	"time"
 )
 
+// Gigasecond constant with package specific duration
+const Gigasecond = 1e9 * time.Second
+
 // AddGigasecond calculates the moment that would be after a gigasecond has passed.
 func AddGigasecond(t time.Time) time.Time {
-	return t.Add(time.Duration(math.Pow(10, 9)) * time.Second)
+	return t.Add(Gigasecond)
 }
