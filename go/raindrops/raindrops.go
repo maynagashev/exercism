c9599b0700aae2c44833b09d835afb6c8faaa0af
Evgeniy <lifesup@gmail.com>
2020-08-02 13:53:39 +0700

    * fix base ex: raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,41 +1,24 @@
 /*
-Raindrops
-
-Your task is to convert a number into a string that contains raindrop sounds corresponding to certain potential factors.
-A factor is a number that evenly divides into another number, leaving no remainder. The simplest way to test if a one
-number is a factor of another is to use the [modulo operation](https://en.wikipedia.org/wiki/Modulo_operation).
-
-The rules of `raindrops` are that if a given number:
-
-- has 3 as a factor, add 'Pling' to the result.
-- has 5 as a factor, add 'Plang' to the result.
-- has 7 as a factor, add 'Plong' to the result.
-- _does not_ have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
+Package raindrops for working with raindrop sounds
 */
 package raindrops
 
 import "strconv"
 
-// isFactor determines whether specified number is a factor of another
-func isFactor(x int, y int) bool {
-	return x%y == 0
-}
-
 // Convert converts integer to raindrops sounds
-func Convert(x int) string {
-	var result string
-	if isFactor(x, 3) {
+func Convert(x int) (result string) {
+	if x%3 == 0 {
 		result += "Pling"
 	}
-	if isFactor(x, 5) {
+	if x%5 == 0 {
 		result += "Plang"
 	}
-	if isFactor(x, 7) {
+	if x%7 == 0 {
 		result += "Plong"
 	}
 
 	if result == "" {
 		result = strconv.Itoa(x)
 	}
-	return result
+	return
 }
