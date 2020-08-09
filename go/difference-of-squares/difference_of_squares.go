144bb13d13d3037f39aed32d24b541012670f90e
Evgeniy <lifesup@gmail.com>
2020-08-09 18:46:54 +0700

    + difference of squares

---
+++ go/difference-of-squares/difference_of_squares.go
@@ -0,0 +1,26 @@
+/*
+Package diffsquares contains functions for calculating difference between the square of the sum and the sum of the squares
+*/
+package diffsquares
+
+// SquareOfSum calculates (1 + 2 + ... + n)²
+func SquareOfSum(n int) int {
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+	return sum * sum
+}
+
+// SumOfSquares calculates 1² + 2² + ... + n²
+func SumOfSquares(n int) (sum int) {
+	for i := 1; i <= n; i++ {
+		sum += i * i
+	}
+	return
+}
+
+// Difference calculates difference between the square of the sum and the sum of the squares of the first N natural numbers
+func Difference(n int) (dif int) {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
