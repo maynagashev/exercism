954e1f0b90789685ed42283ad0bf47e05ca085c7
Evgeniy <lifesup@gmail.com>
2020-08-10 12:05:44 +0700

    * reversed map to switch, after review
    Before:
    BenchmarkCodon
    BenchmarkCodon-12         608200              1945 ns/op               0 B/op          0 allocs/op
    BenchmarkProtein
    BenchmarkProtein-12       380451              2916 ns/op             400 B/op         12 allocs/op
    
    
    After:
    BenchmarkCodon
    BenchmarkCodon-12        6530343               188 ns/op               0 B/op          0 allocs/op
    BenchmarkProtein
    BenchmarkProtein-12      1381718               870 ns/op             400 B/op         12 allocs/op

--- go/protein-translation/protein_translation.go
+++ go/protein-translation/protein_translation.go
@@ -8,31 +8,31 @@ var (
 	ErrStop = errors.New("stop codon")
 	// ErrInvalidBase error when codon is invalid and not found in base
 	ErrInvalidBase = errors.New("invalid codon base")
-
-	proteins = map[string][]string{
-		"Methionine":    {"AUG"},
-		"Phenylalanine": {"UUU", "UUC"},
-		"Leucine":       {"UUA", "UUG"},
-		"Serine":        {"UCU", "UCC", "UCA", "UCG"},
-		"Tyrosine":      {"UAU", "UAC"},
-		"Cysteine":      {"UGU", "UGC"},
-		"Tryptophan":    {"UGG"},
-		"STOP":          {"UAA", "UAG", "UGA"},
-	}
 )
 
 // FromCodon translates codon to protein
-func FromCodon(codon string) (protein string, err error) {
-	for p, codons := range proteins {
-		for _, c := range codons {
-			if codon == c && p == "STOP" {
-				return "", ErrStop
-			} else if codon == c {
-				return p, nil
-			}
-		}
+func FromCodon(codon string) (p string, err error) {
+	switch codon {
+	case "AUG":
+		p = "Methionine"
+	case "UUU", "UUC":
+		p = "Phenylalanine"
+	case "UUA", "UUG":
+		p = "Leucine"
+	case "UCU", "UCC", "UCA", "UCG":
+		p = "Serine"
+	case "UAU", "UAC":
+		p = "Tyrosine"
+	case "UGU", "UGC":
+		p = "Cysteine"
+	case "UGG":
+		p = "Tryptophan"
+	case "UAA", "UAG", "UGA": // STOP
+		return "", ErrStop
+	default:
+		return "", ErrInvalidBase
 	}
-	return "", ErrInvalidBase
+	return p, nil
 }
 
 // FromRNA translates RNA sequence to protein sequence
