dcaf4ff027b7f80a387d1c6f989fdb4c0a28234c
Evgeniy <lifesup@gmail.com>
2020-08-16 23:52:41 +0700

    + benchmarks

--- go/main.go
+++ go/main.go
@@ -1,24 +1,60 @@
+/*
+Package cryptosquare contains the classic method for composing secret messages called a square code.
+
+First, the input is normalized: the spaces and punctuation are removed from the English text and the message is downcased.
+Then, the normalized characters are broken into rows. These rows can be regarded as forming a rectangle when printed with intervening newlines.
+The plaintext should be organized in to a rectangle. The size of the rectangle (r x c) should be decided by the length of the message,
+such that c >= r and c - r <= 1, where c is the number of columns and r is the number of rows.
+*/
 package main
 
 import (
-	"fmt"
+	"math"
+	"strings"
 	"unicode"
 )
 
 func main() {
-	IsPangram("Five quacking Zephyrs jolt my wax bed.")
+	Normalize("First, the input is normalized: the spaces and punctuation are removed from the English text and the message is downcased.")
 }
 
-// IsPangram determines if the given string contains every letter
-func IsPangram(s string) bool {
-	var bs int32
-	for i := 0; i < len(s); i++ {
-		fmt.Printf("%v %c %v %v %c %+v %T\n", unicode.ToUpper(rune(i)), s[i], s[i], 0xdf, s[i] & 0xdf, s[i] & 0xdf, (s[i] & 0xdf))
-		c := (s[i] & 0xdf) - 'A'
-		if c > 25 || c < 0 {
-			continue
-		}
-		bs |= 1 << c
+
+// Encode encodes string with square code algorithm.
+func Encode(s string) string {
+	var c, r int
+
+	s = Normalize(s)
+	if s == "" {
+		return ""
 	}
-	return bs == 0x3ffffff
+	sqrt := math.Sqrt(float64(len(s)))
+
+	c = int(math.Round(sqrt))
+	if sqrt > math.Round(sqrt) {
+		r = c + 1
+	} else {
+		r = c
+	}
+	for i := r*c - len(s); i > 0; i-- {
+		s = s + " "
+	}
+	builder := strings.Builder{}
+	for i := 0; i < r; i++ {
+		for j := 0; j < c; j++ {
+			builder.WriteByte(s[i+(j*r)])
+		}
+		builder.WriteRune(' ')
+	}
+	s = builder.String()
+	return s[:len(s)-1]
+}
+func Normalize(s string) string {
+	s = strings.ToLower(s)
+	builder := strings.Builder{}
+	for _, el := range s {
+		if unicode.IsDigit(el) || unicode.IsLetter(el) {
+			builder.WriteRune(el)
+		}
+	}
+	return builder.String()
 }
