fb213434f086713ab1c030a88fb4a2e5ce589cb6
Evgeniy <lifesup@gmail.com>
2020-08-17 13:04:53 +0700

    ++ crypto square

--- go/crypto-square/crypto_square.go
+++ go/crypto-square/crypto_square.go
@@ -3,53 +3,51 @@ Package cryptosquare contains the classic method for composing secret messages c
 
 First, the input is normalized: the spaces and punctuation are removed from the English text and the message is downcased.
 Then, the normalized characters are broken into rows. These rows can be regarded as forming a rectangle when printed with intervening newlines.
-The plaintext should be organized in to a rectangle. The size of the rectangle (r x c) should be decided by the length of the message,
-such that c >= r and c - r <= 1, where c is the number of columns and r is the number of rows.
 */
-package main
+package cryptosquare
 
 import (
-	"fmt"
 	"math"
 	"strings"
 	"unicode"
 )
 
-func main() {
-	fmt.Println(Encode("ZOMG! ZOMBIES!!!"))
-}
-
 // Encode encodes string with square code algorithm.
-func Encode(s string) (res string) {
-	var c, r int
-	s = normalize(s)
+func Encode(s string) string {
 	if s == "" {
 		return ""
 	}
-	r, c = rectDims(len(s))
-	fmt.Println(s, len(s), c, r)
 
-	// add trailing space to input string?
-	for i := r*c - len(s); i > 0; i-- {
-		s = s + " "
-	}
+	s = normalize(s)
+	r, c := rectDims(len(s))
 
 	builder := strings.Builder{}
+	builder.Grow(len(s))
+	// resulting rows count = count of input rectangle cols (c)
 	for resRow := 0; resRow < c; resRow++ {
+		// resulting cols count = count of input rectangle rows (r)
 		for resCol := 0; resCol < r; resCol++ {
-			builder.WriteByte(s[resRow+resCol*c])
+			// pick every c-th symbol (resCol*c) in current resRow
+			i := resCol*c + resRow
+			// if current index out of bounds (string ended), then add trailing space in resulting row
+			if i >= len(s) {
+				builder.WriteByte(' ')
+				continue
+			}
+			builder.WriteByte(s[i])
 		}
-		builder.WriteRune(' ')
-	}
-	res = builder.String()
 
-	return res
+		// output chunks (rows) separator (space symbol), without trailing space
+		if resRow < c-1 {
+			builder.WriteRune(' ')
+		}
+	}
+
+	return builder.String()
 }
 
+// normalize removes all non-letter and non-digit runes from given string
 func normalize(s string) string {
-	if s == "" {
-		return ""
-	}
 	builder := strings.Builder{}
 	builder.Grow(len(s))
 	for _, el := range s {
@@ -60,12 +58,15 @@ func normalize(s string) string {
 	return builder.String()
 }
 
+// rectDims calculates dimensions for source rectangle
+// The size of the rectangle (r x c) should be decided by the length of the message,
+// such that c >= r and c - r <= 1, where c is the number of columns and r is the number of rows.
 func rectDims(l int) (r, c int) {
 	sqrt := math.Sqrt(float64(l))
 	roundSqrt := math.Round(sqrt)
 	r = int(roundSqrt)
 	c = int(roundSqrt)
-	if  sqrt > roundSqrt {
+	if sqrt > roundSqrt {
 		c = r + 1
 	}
 	return
--- go/crypto-square/crypto_square_test.go
+++ go/crypto-square/crypto_square_test.go
@@ -1,4 +1,4 @@
-package main
+package cryptosquare
 
 import "testing"
 
@@ -84,17 +84,17 @@ var tests = []struct {
 	},
 }
 
-//func TestEncode(t *testing.T) {
-//	for _, test := range tests {
-//		if ct := Encode(test.pt); ct != test.ct {
-//			t.Fatalf(`Encode(%q):
-//got  %q
-//want %q`, test.pt, ct, test.ct)
-//		}
-//	}
-//}
+func TestEncode(t *testing.T) {
+	for _, test := range tests {
+		if ct := Encode(test.pt); ct != test.ct {
+			t.Fatalf(`Encode(%q):
+got  %q
+want %q`, test.pt, ct, test.ct)
+		}
+	}
+}
 
-func BenchmarkNormalize(b *testing.B) {
+func BenchmarkEncode(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		for _, test := range tests {
 			Encode(test.pt)
