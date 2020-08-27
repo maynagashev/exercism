7d4b0a1ed457fe8067136329753af08cd3f7a142
Evgeniy <lifesup@gmail.com>
2020-08-28 02:09:58 +0700

    + sieve

--- go/main.go
+++ go/main.go
@@ -1,24 +1,12 @@
-/*
-a-z 97-122
-97-122
-98-121
-99-120
-*/
 package main
 
-import "fmt"
-
 func main() {
-	Atbash("yesyes")
+	Sieve(20)
 }
+func Sieve(limit int) (primes []int) {
 
-func Atbash(s string) (res string) {
-	b:= make([]byte, len(s))
-	for i := 0; i < len(s); i++ {
-		shift:= s[i]-'a'
-		b[i] ='z'-shift
-		fmt.Printf("%#v %d %T %+v %d %d %s\n", s[i], s[i], s[i], i, shift, b, string(b))
+	for i := 2; i < limit; i++ {
+		primes = append(primes, i)
 	}
-	fmt.Println(b, string(b))
-	return string(b)
+	return
 }
--- go/sieve/sieve.go
+++ go/sieve/sieve.go
@@ -1,1 +1,49 @@
+/*
+Package sieve uses the Sieve of Eratosthenes algorithm to find all the primes from 2 up to a given number.
+
+Algorithm:
+
+- take the next available unmarked number in your list (it is prime)
+- mark all the multiples of that number (they are not prime - composite)
+
+Benchmarks:
+
+1. primes := make([]int, 0)
+BenchmarkSieve
+BenchmarkSieve-12    	  269534	      3897 ns/op	    5336 B/op	      22 allocs/op
+
+2. primes := make([]int, 0, limit)
+BenchmarkSieve
+BenchmarkSieve-12    	  382113	      3019 ns/op	    9472 B/op	      10 allocs/op
+
+3. primes := make([]int, 0, limit*4/10)
+BenchmarkSieve
+BenchmarkSieve-12         522270              2299 ns/op            4432 B/op         10 allocs/op
+
+4. iterate only through odd numbers
+BenchmarkSieve
+BenchmarkSieve-12         696003              1707 ns/op            4432 B/op         10 allocs/op
+
+*/
 package sieve
+
+// Sieve finds all the primes from 2 up to a given number.
+func Sieve(limit int) []int {
+	composite := make([]bool, limit+1)
+	primes := make([]int, 0, limit*4/10)
+
+	if limit > 1 {
+		primes = append(primes, 2)
+	}
+	// further check only "odd" numbers
+	for i := 3; i <= limit; i += 2 {
+		if composite[i] {
+			continue
+		}
+		primes = append(primes, i)
+		for j := i * 2; j <= limit; j += i {
+			composite[j] = true
+		}
+	}
+	return primes
+}
