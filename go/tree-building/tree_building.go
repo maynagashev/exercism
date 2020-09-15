a6a890c615e6338f7cbada136d917e416175163b
Evgeniy <lifesup@gmail.com>
2020-09-15 23:23:18 +0700

    + comments to tree

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -1,15 +1,26 @@
+/*
+Package tree contains Build method that implements logic for building tree structure from slice of records.
+*/
 package tree
 
+// Record struct representing single record
 type Record struct {
-	ID int
+	ID     int
 	Parent int
 }
 
+// Node struct representing result tree
 type Node struct {
-	ID int
+	ID       int
 	Children []*Node
 }
 
+// Build builds tree of Node-s from slice of records
 func Build(records []Record) (*Node, error) {
+
+	if len(records) == 0 {
+		return nil, nil
+	}
+
 	return &Node{}, nil
 }
