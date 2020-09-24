62c75e169cbcf7de5c7a4a34a16841a8d5a0be07
Evgeniy <lifesup@gmail.com>
2020-09-24 09:45:10 +0700

    * pass base tests, comments in robot name

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -5,24 +5,36 @@ import (
 	"math/rand"
 )
 
+// Robot with name
 type Robot struct {
+	name string
 }
 
-const max = 26 * 26 * 10 * 10 * 10
-const period = 26 + 26 + 10 + 10 + 10
+const maxNameVariants = 26 * 26 * 10 * 10 * 10
 
-func (*Robot) Name() (string, error) {
-	rnd := rand.Intn(max)
+// Name returns current Robot name
+// If name is empty, then will be generated new name
+func (r *Robot) Name() (string, error) {
+	if r.name == "" {
+		r.name = newName()
+	}
+	return r.name, nil
+}
+
+// Reset clears current name of Robot
+func (r Robot) Reset() {
+
+}
+
+// newName generates new random name for Robot
+func newName() (name string) {
+	rnd := rand.Intn(maxNameVariants)
 	rndDigit := rand.Intn(1000)
-	fmt.Println("max: ", max, "rnd: ", rnd, (rnd+26)%rnd, "rndDigit:", rndDigit)
+	fmt.Println("newName() maxNameVariants: ", maxNameVariants, "rnd (full): ", rnd, (rnd+26)%rnd, "rndDigit:", rndDigit)
 	d1 := rndDigit/100 - rndDigit/100/10*10
 	d2 := rndDigit/10 - rndDigit/10/10*10
 	d3 := rndDigit - rndDigit/10*10
-	res := "AA" + string(rune('0'+d1)) + string(rune('0'+d2)) + string(rune('0'+d3))
-	fmt.Println(res)
-	return res, nil
-}
-
-func (r Robot) Reset() {
-
+	name = "AA" + string(rune('0'+d1)) + string(rune('0'+d2)) + string(rune('0'+d3))
+	fmt.Println(name)
+	return name
 }
