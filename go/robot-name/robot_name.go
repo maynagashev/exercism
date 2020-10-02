d091086e2028cf4565c1084e84051178a4d2f9d6
Evgeniy <lifesup@gmail.com>
2020-10-03 00:23:40 +0700

    * done robot name

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -14,25 +14,21 @@ type Robot struct {
 const max = 26 * 26 * 10 * 10 * 10
 
 var used = make(map[string]bool)
+var random = rand.New(rand.NewSource(time.Now().UnixNano()))
 
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
-// Name returns current Robot name
-// If name is empty, then will be generated new name (not used before)
+// Name returns current Robot name or generates new unique name (not used before).
 func (r *Robot) Name() (name string, err error) {
-	if r.name == "" {
-		if len(used) >= max {
-			return "", fmt.Errorf("generated %d names, no more variants available", len(used))
-		}
-		r.name = newName()
-		for used[r.name] {
-			r.name = newName()
-		}
-		used[r.name] = true
+	if r.name != "" {
+		return r.name, err
 	}
-
+	if len(used) >= max {
+		return "", fmt.Errorf("generated %d names, no more variants available", len(used))
+	}
+	r.name = newName()
+	for used[r.name] {
+		r.name = newName()
+	}
+	used[r.name] = true
 	return r.name, err
 }
 
@@ -43,8 +39,8 @@ func (r *Robot) Reset() {
 
 // newName returns new random name for the Robot
 func newName() string {
-	r1 := rand.Intn(26) + 'A'
-	r2 := rand.Intn(26) + 'A'
-	num := rand.Intn(1000)
+	r1 := random.Intn(26) + 'A'
+	r2 := random.Intn(26) + 'A'
+	num := random.Intn(1000)
 	return fmt.Sprintf("%c%c%03d", r1, r2, num)
 }
