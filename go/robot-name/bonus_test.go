761b541ed1a1af8b0600ef82fe01035dfcbb6386
Evgeniy <lifesup@gmail.com>
2020-10-02 13:39:22 +0700

    + measurements

--- go/robot-name/bonus_test.go
+++ go/robot-name/bonus_test.go
@@ -2,14 +2,29 @@
 
 package robotname
 
-import "testing"
+import (
+	"testing"
+	"time"
+	"fmt"
+)
 
 var maxNames = 26 * 26 * 10 * 10 * 10
 
+func elapsed(what string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", what, time.Since(start))
+	}
+}
+
 func TestCollisions(t *testing.T) {
 	// Test uniqueness for new robots.
-	for i := len(seen); i <= maxNames-600000; i++ {
+	for i := len(seen); i < maxNames; i++ {
+		start := time.Now()
 		_ = New().getName(t, false)
+		if i > maxNames-10 || i<10{
+			fmt.Printf("iteration %d took %v\n", i, time.Since(start))
+		}
 	}
 
 	// Test that names aren't recycled either.
