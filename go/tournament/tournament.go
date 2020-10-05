4464b3d5a3c5ce6e4fadf7e63f42a823187e9626
Evgeniy <lifesup@gmail.com>
2020-10-05 13:32:05 +0700

    + scan lines

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -1,15 +1,18 @@
 package tournament
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 )
 
+// Tally writes to specified writer the number of points, goals, runs, etc. achieved in a game or by a team.
 func Tally(r io.Reader, w io.Writer) error {
-	p := make([]byte, 100)
-	fmt.Println(r.Read(p))
-	fmt.Println(string(p))
-	fmt.Println(r.Read(p))
-	fmt.Println(string(p))
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		t := scanner.Text()
+		fmt.Println(t)
+		w.Write([]byte(t))
+	}
 	return nil
 }
