7049533f48595f39e73ed32339e57e01dd20267e
Evgeniy <lifesup@gmail.com>
2020-10-07 00:29:02 +0700

    + header

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -9,6 +9,7 @@ import (
 // Tally writes to specified writer the number of points, goals, runs, etc. achieved in a game or by a team.
 func Tally(r io.Reader, w io.Writer) error {
 	scanner := bufio.NewScanner(r)
+	w.Write([]byte("Team                           | MP |  W |  D |  L |  P\n"))
 	for scanner.Scan() {
 		t := scanner.Text()
 		fmt.Println(t)
