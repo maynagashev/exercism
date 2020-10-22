443bf2e8b664fde3a8a771e4f932acf859e1be4d
Evgeniy <lifesup@gmail.com>
2020-10-23 00:14:08 +0700

    * refactoring slice

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -8,31 +8,35 @@ import (
 	"strings"
 )
 
-type team struct {
+// Team representation of single team data
+type Team struct {
+	name                                 string
 	matches, wins, losses, draws, points int
 }
-type TeamMap map[string]team
 
-type sortedTeam struct {
-	name   string
-	points int
-}
-type League []sortedTeam
+// TeamMap map of teams by names
+type TeamMap map[string]Team
+
+// League is sorted slice of teams
+type League []Team
 
 func (p League) Len() int      { return len(p) }
 func (p League) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
 func (p League) Less(i, j int) bool {
 	return p[i].points > p[j].points || (p[i].points == p[j].points && p[i].name < p[j].name)
 }
+
+// LeagueFromTeams converts TeamMap to sorted slice League
 func LeagueFromTeams(teams TeamMap) League {
-	slice := make(League, 0, len(teams))
-	for name, team := range teams {
-		slice = append(slice, sortedTeam{name, team.points})
+	league := make(League, 0, len(teams))
+	for _, team := range teams {
+		league = append(league, team)
 	}
-	sort.Sort(slice)
-	return slice
+	sort.Sort(league)
+	return league
 }
 
+// ReadMatchDataFrom parses match data using team map
 func ReadMatchDataFrom(r io.Reader) (TeamMap, error) {
 	teams := TeamMap{}
 	scanner := bufio.NewScanner(r)
@@ -53,7 +57,7 @@ func ReadMatchDataFrom(r io.Reader) (TeamMap, error) {
 		name1, name2, result := f[0], f[1], f[2]
 
 		a, b := teams[name1], teams[name2]
-
+		a.name, b.name = name1, name2
 		a.matches++
 		b.matches++
 		switch result {
@@ -79,6 +83,7 @@ func ReadMatchDataFrom(r io.Reader) (TeamMap, error) {
 	return teams, nil
 }
 
+// WriteLeagueTableTo prints teams from league as a table
 func WriteLeagueTableTo(w io.Writer, league League) (err error) {
 	const format = "%-30v |%3v |%3v |%3v |%3v |%3v\n"
 
@@ -86,9 +91,8 @@ func WriteLeagueTableTo(w io.Writer, league League) (err error) {
 	if err != nil {
 		return err
 	}
-	for _, s := range league {
-		t := teams[s.name]
-		_, err := fmt.Fprintf(w, format, s.name, t.matches, t.wins, t.draws, t.losses, t.points)
+	for _, t := range league {
+		_, err := fmt.Fprintf(w, format, t.name, t.matches, t.wins, t.draws, t.losses, t.points)
 		if err != nil {
 			return err
 		}
@@ -97,7 +101,7 @@ func WriteLeagueTableTo(w io.Writer, league League) (err error) {
 	return nil
 }
 
-// Tally creates teams map with team structs
+// Tally creates teams map with Team structs
 func Tally(r io.Reader, w io.Writer) error {
 	teams, err := ReadMatchDataFrom(r)
 	if err != nil {
