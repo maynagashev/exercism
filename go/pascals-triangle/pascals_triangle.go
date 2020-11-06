20cf14cc76033fac367d473242eb36fb5cf787eb
Evgeniy <lifesup@gmail.com>
2020-11-06 23:51:04 +0700

    + pascals triangle solution

--- go/pascals-triangle/pascals_triangle.go
+++ go/pascals-triangle/pascals_triangle.go
@@ -1,1 +1,22 @@
 package pascal
+
+// Triangle computes Pascal's triangle up to a given number of rows
+func Triangle(n int) (resultRows [][]int) {
+	for i := 0; i < n; i++ {
+		row := []int{1}
+		if i-1 < 0 {
+			resultRows = append(resultRows, row)
+			continue
+		}
+		prevRow := resultRows[i-1]
+		for j := 1; j <= i; j++ {
+			samePosValue := 0
+			if j < len(prevRow) {
+				samePosValue = prevRow[j]
+			}
+			row = append(row, prevRow[j-1]+samePosValue)
+		}
+		resultRows = append(resultRows, row)
+	}
+	return
+}
