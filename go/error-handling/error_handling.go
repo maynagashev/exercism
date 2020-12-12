31c5c4e488d878c89dca6e0c55bb78eda4e840b6
Evgeniy <lifesup@gmail.com>
2020-12-12 23:21:45 +0700

    + open and call frob

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -1,1 +1,13 @@
 package erratum
+
+func Use(o ResourceOpener, input string) error {
+	resource, err := o()
+	if err != nil {
+		return err
+	}
+	defer resource.Close()
+
+	resource.Frob(input)
+
+	return nil
+}
