d1b31c20b96547cbd5537e5d3845d251f050054e
Evgeniy <lifesup@gmail.com>
2021-01-23 20:22:12 +0700

    + error handling

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -1,11 +1,30 @@
 package erratum
 
-func Use(o ResourceOpener, input string) error {
+import "fmt"
+
+func Use(o ResourceOpener, input string) (err error) {
 	resource, err := o()
-	if err != nil {
+	for err != nil {
+		if _, ok := err.(TransientError); ok {
+			resource, err = o()
+			continue
+		}
 		return err
 	}
 	defer resource.Close()
+	defer func() {
+		if r := recover(); r != nil {
+			switch e := r.(type) {
+			case FrobError:
+				resource.Defrob(e.defrobTag)
+				err = e
+			case error:
+				err = e
+			default:
+				err = fmt.Errorf("%v", r)
+			}
+		}
+	}()
 
 	resource.Frob(input)
 
