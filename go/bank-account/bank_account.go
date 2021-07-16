4257a6aaeab37fb3209f1646d72f1e100ba63294
Evgeniy <lifesup@gmail.com>
2021-07-16 11:17:42 +0700

    + account boilerplate

--- go/bank-account/bank_account.go
+++ go/bank-account/bank_account.go
@@ -2,7 +2,6 @@ package account
 
 // Account is bank account entity
 type Account struct {
-
 }
 
 // Open creates new bank account
@@ -10,6 +9,17 @@ func Open(initialDeposit int64) *Account {
 	return &Account{}
 }
 
-func (*Account) Balance() {
+// Balance returns current state of account
+func (*Account) Balance() (balance int64, ok bool) {
+	return 0, true
+}
 
+// Deposit increments balance
+func (*Account) Deposit(amount int64) (newBalance int64, ok bool) {
+	return 0, true
+}
+
+// Close closes account
+func (*Account) Close() (payout int64, ok bool) {
+	return 0, true
 }
