8ba08035e2dbc7273aadf8c3c044add087fc11fc
Evgeniy <lifesup@gmail.com>
2021-07-22 23:53:09 +0700

    + pass first tests

--- go/bank-account/bank_account.go
+++ go/bank-account/bank_account.go
@@ -2,24 +2,45 @@ package account
 
 // Account is bank account entity
 type Account struct {
+	balance  int64
+	isClosed bool
 }
 
 // Open creates new bank account
 func Open(initialDeposit int64) *Account {
-	return &Account{}
+	if initialDeposit < 0 {
+		return nil
+	}
+	var a = &Account{isClosed: false}
+	a.Deposit(initialDeposit)
+	return a
 }
 
 // Balance returns current state of account
-func (*Account) Balance() (balance int64, ok bool) {
-	return 0, true
+func (a *Account) Balance() (balance int64, ok bool) {
+	if a.isClosed {
+		return a.balance, false
+	}
+	return a.balance, true
 }
 
 // Deposit increments balance
-func (*Account) Deposit(amount int64) (newBalance int64, ok bool) {
-	return 0, true
+func (a *Account) Deposit(amount int64) (newBalance int64, ok bool) {
+	if a.isClosed {
+		return a.balance, false
+	}
+	if a.balance+amount < 0 {
+		return a.balance, false
+	}
+
+	a.balance += amount
+	return a.balance, true
 }
 
 // Close closes account
-func (*Account) Close() (payout int64, ok bool) {
-	return 0, true
+func (a *Account) Close() (payout int64, ok bool) {
+	payout = a.balance
+	a.balance = 0
+	a.isClosed = true
+	return payout, true
 }
