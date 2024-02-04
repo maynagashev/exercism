efb8a56c870b23b8b4c3f9dca4ad9b96417061ba
Dev <lifesup@gmail.com>
2024-02-04 21:20:16 +0700

    flatten nested arrays

--- go/flatten-array/flatten_array.go
+++ go/flatten-array/flatten_array.go
@@ -1,13 +1,21 @@
 package flatten
 
-import "fmt"
-
-// Flatten flattens nested array
-func Flatten(input interface{}) (flattened []interface{}) {
-	fmt.Printf("Input: %+v\n", input)
-	for _, v := range input.([]interface{}) {
-		flattened = append(flattened, v)
+func Flatten(input interface{}) []interface{} {
+	var result = make([]interface{}, 0)
+	//fmt.Printf("Input: %+v\n", input)
+	for _, value := range input.([]interface{}) {
+		switch v := value.(type) {
+		case []interface{}:
+			// If the element is a slice, recursively flatten it and append the results.
+			result = append(result, Flatten(v)...)
+		case nil:
+			// Ignore nil values.
+			continue
+		default:
+			// Append non-nil, non-slice values directly to the result.
+			result = append(result, v)
+		}
 	}
-	//fmt.Printf("Flattened: %+v\n", flattened)
-	return flattened
+
+	return result
 }
