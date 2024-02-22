e8b9ea321b9991accde319f1f944502548d2ceb6
Dev <lifesup@gmail.com>
2024-02-22 23:10:51 +0700

    house

--- go/house/house.go
+++ go/house/house.go
@@ -1,9 +1,38 @@
 package house
 
-func Verse(v int) string {
-	panic("Please implement the Verse function")
+// lines contains the unique parts of each verse in reverse order.
+var lines = []string{
+	"the house that Jack built.",
+	"the malt\nthat lay in ",
+	"the rat\nthat ate ",
+	"the cat\nthat killed ",
+	"the dog\nthat worried ",
+	"the cow with the crumpled horn\nthat tossed ",
+	"the maiden all forlorn\nthat milked ",
+	"the man all tattered and torn\nthat kissed ",
+	"the priest all shaven and shorn\nthat married ",
+	"the rooster that crowed in the morn\nthat woke ",
+	"the farmer sowing his corn\nthat kept ",
+	"the horse and the hound and the horn\nthat belonged to ",
 }
 
+// Verse generates a single verse of the song.
+func Verse(v int) string {
+	verse := "This is "
+	for i := v - 1; i >= 0; i-- {
+		verse += lines[i]
+	}
+	return verse
+}
+
+// Song generates the entire song.
 func Song() string {
-	panic("Please implement the Song function")
+	song := ""
+	for i := 1; i <= len(lines); i++ {
+		if i > 1 {
+			song += "\n\n"
+		}
+		song += Verse(i)
+	}
+	return song
 }
