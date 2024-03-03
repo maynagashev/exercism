4c0f24b844e5f17bf6920e0548766112d60d06b6
Dev <lifesup@gmail.com>
2024-03-03 12:29:08 +0700

    passio mutex

--- go/paasio/paasio.go
+++ go/paasio/paasio.go
@@ -1,35 +1,75 @@
 package paasio
 
-import "io"
+import (
+	"io"
+	"sync"
+)
 
 // Define readCounter and writeCounter types here.
+type readCounter struct {
+	reader   io.Reader
+	bytes    int64
+	count    int64
+	readLock sync.Mutex
+}
+type writeCounter struct {
+	writer     io.Writer
+	writeBytes int64
+	writeCount int64
+	writeLock  sync.Mutex
+}
+type readWriteCounter struct {
+	readCounter
+	writeCounter
+}
 
 // For the return of the function NewReadWriteCounter, you must also define a type that satisfies the ReadWriteCounter interface.
 
 func NewWriteCounter(writer io.Writer) WriteCounter {
-	panic("Please implement the NewWriterCounter function")
+	return &writeCounter{writer, 0, 0, sync.Mutex{}}
 }
 
 func NewReadCounter(reader io.Reader) ReadCounter {
-	panic("Please implement the NewReadCounter function")
+	return &readCounter{reader, 0, 0, sync.Mutex{}}
 }
 
 func NewReadWriteCounter(readwriter io.ReadWriter) ReadWriteCounter {
-	panic("Please implement the NewReadWriteCounter function")
+	t := new(readWriteCounter)
+	t.reader = NewReadCounter(readwriter)
+	t.writer = NewWriteCounter(readwriter)
+	return t
 }
 
 func (rc *readCounter) Read(p []byte) (int, error) {
-	panic("Please implement the Read function")
+	n, err := rc.reader.Read(p)
+	rc.readLock.Lock()
+	rc.bytes += int64(n)
+	rc.count++
+	rc.readLock.Unlock()
+	return n, err
 }
 
 func (rc *readCounter) ReadCount() (int64, int) {
-	panic("Please implement the ReadCount function")
+	rc.readLock.Lock()
+	defer func() {
+		rc.readLock.Unlock()
+	}()
+	return rc.bytes, int(rc.count)
 }
 
 func (wc *writeCounter) Write(p []byte) (int, error) {
-	panic("Please implement the Write function")
+	n, err := wc.writer.Write(p)
+	wc.writeLock.Lock()
+	wc.writeBytes += int64(n)
+	wc.writeCount++
+	wc.writeLock.Unlock()
+	return n, err
 }
 
 func (wc *writeCounter) WriteCount() (int64, int) {
-	panic("Please implement the WriteCount function")
+	wc.writeLock.Lock()
+	defer func() {
+		wc.writeLock.Unlock()
+	}()
+	return wc.writeBytes, int(wc.writeCount)
 }
