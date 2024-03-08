a426bbf375bf0892a34a8a19d02908c8c5f07a9d
Dev <lifesup@gmail.com>
2024-03-08 08:27:07 +0700

    binary search tree

--- go/binary-search-tree/binary_search_tree.go
+++ go/binary-search-tree/binary_search_tree.go
@@ -8,13 +8,25 @@ type BinarySearchTree struct {
 
 // NewBst creates and returns a new BinarySearchTree.
 func NewBst(i int) *BinarySearchTree {
-	panic("Please implement the NewBst function")
+	return &BinarySearchTree{data: i}
 }
 
 // Insert inserts an int into the BinarySearchTree.
 // Inserts happen based on the rules of a binary search tree
 func (bst *BinarySearchTree) Insert(i int) {
-	panic("Please implement the Insert method")
+	if i <= bst.data {
+		if bst.left == nil {
+			bst.left = NewBst(i)
+		} else {
+			bst.left.Insert(i)
+		}
+	} else {
+		if bst.right == nil {
+			bst.right = NewBst(i)
+		} else {
+			bst.right.Insert(i)
+		}
+	}
 }
 
 // SortedData returns the ordered contents of BinarySearchTree as an []int.
@@ -22,5 +34,8 @@ func (bst *BinarySearchTree) Insert(i int) {
 // A BinarySearchTree that has the numbers [1,3,7,5] added will return the
 // []int [1,3,5,7].
 func (bst *BinarySearchTree) SortedData() []int {
-	panic("Please implement the SortedData function")
+	if bst == nil {
+		return []int{}
+	}
+	return append(append(bst.left.SortedData(), bst.data), bst.right.SortedData()...)
 }
